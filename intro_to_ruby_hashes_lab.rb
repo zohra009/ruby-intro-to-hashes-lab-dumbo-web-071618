a9a8b50388ef1f23a7255797895e9ffc90728bd4
anzohra@outlook.com <anzohra@outlook.com>
2018-07-10 13:17:29 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,15 @@ def monopoly_with_third_tier
 	  railroads: {
       pieces: 4, 
       names: {}, 
-      rent_in_dollars: {}
+      rent_in_dollars: {
+   one_piece_owned: 25,
+    two_pieces_owned: 50,
+      three_pieces_owned: 100,
+      four_pieces_owned: 200},
+        names:
+        {
+      
+      }
 	}}	
 end
 
